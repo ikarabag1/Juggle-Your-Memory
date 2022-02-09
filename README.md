ffa59c50c0a72cb9aa663f053a979080f965b876
“ikarabag1” <irmakkarabag@gmail.com>
2022-02-09 07:15:06 +0300

    Game logic done & only announcement, score boards and reset game left

--- README.md
+++ README.md
@@ -5,14 +5,14 @@
     * Test your memorizing skills with flipping cards & matching most of them in a deck of cards.
 
 **WireFrames:**
-![WireFrameimg](./img/WireFrame.jpg)
+
 * We will create a 4x4 grid game board layout with Javascript.
-* Landing page.
-* Game start page.
-* Game ongoing page.
-* Game over page.
+* Landing page.![WireFrameimg](wireframe/board-img/landing-page.jpg)
+* Game start page.![WireFrameimg](wireframe/board-img/game-start-page.jpg)
+* Game ongoing page.![WireFrameimg](wireframe/board-img/game-active.jpg)
+* Game over page.![WireFrameimg](wireframe/board-img/error-game-over-page.jpg)
 * CSS styling will be used.
-* JS for funtionality.
+* JS DOM for funtionality.
 
 **Instructions:**
 
@@ -22,18 +22,19 @@
 
 * Build a 4x4 board for cards.
 * Build matching pairs of cards face down.
-* DOM API funtionality will run.
+* Have a landing page with objectives and start game button.
+* DOM funtionality will run.
 * Select a random image for each pairs of cards. 
 * Append those in to the DOM. 
 * Classes will be assigned to cards. 
 * Event Listeners will be added according to the classes of card elements. 
 * When a card clicked, cards will be flipped & expose image.
-* Build a score table. 
 * Game Logic will be added.
     - When cards are not pairs (false), error color surrounds borders and flip back. 
     - Cards matched (true) fade away.
+* Build a score table. 
 * Update score table when cards matched.
-* Announce how many cards matched.
+* Announce if matched or not and how many cards matched.
 * Game over button function will reset the game.
     - clear the score table.
     - flip the cards back.
@@ -42,6 +43,7 @@
 
 * Build countdown timer to 1 min.
 * Announce amount of matched pairs at given time.
+* Shuffle cards each game.
 * Adding animation to fading away of cards. 
 * Adding sound to matched pairs.
 
--- game.js
+++ game.js
@@ -2,75 +2,123 @@
 // a list of all card elements and store it under cards
 const cards = document.querySelectorAll('.card');
 const board = document.getElementById('game-board');
-let backCards = document.querySelectorAll('.back');
-const frontCards = document.querySelectorAll('.front');
+const back = document.querySelectorAll('.back');
+const front = document.querySelectorAll('.front');
+const reset = document.querySelector('reset-btn');
+console.log(cards)
 
-const backCardsArray = ['cork', 'book', 'mug', 'wallet', 'key', 'fedora', 'remote', ' book', 'key', 'bottleopener', 'cork', 'book', 'mug', 'wallet']
+let count=0; //track of flipped cards
 
-// compare values of flipped cards first and second click
-let flippedCards = [];
+// generate data
+const backCardsArrayId = ['cork', 'book', 'mug', 'wallet', 'key', 'fedora', 'remote', ' book', 'key', 'bottleopener', 'cork', 'book', 'mug', 'wallet']
 
-// matched ones
-let matched = [];
 
 // score array
 let score = [];
 
+// LANDING PAGE - INFO
+
 
 // GAME START 
 function gameStartBoard() {
-    cardsFlipped = 0;
-    flippedCards = [];
-    for (let i = backCardsArray.length - 1; i < 0; i++) {
+    shuffleCards();
+}
+// gameStartBoard()
 
-    } 
+// // RANDOMIZING CARDS
+function shuffleCards () {
+backCardsArrayId.sort(() => Math.random() - 0.5);
+console.log(backCardsArrayId)
 }
 
-// FLIIPED CARD FUNCTION clicked and flipped and loop thrugh cards 
-function flipCard () {
+   // add eventListener to each card
+   cards.forEach(card =>card.addEventListener('click', flipCard)); 
+
+   // FLIPPING CARDS FUNCTION 
+function flipCard (event) {
+ console.log('card flipped');
+ console.log(event);
+// add classList flip
     this.classList.add('flip');
-
-cards.forEach(card =>card.addEventListener('click', flipCard));
-
-// conditionals cghecked for matched pairs 
-if (flippedCards.length == 0) {
-    flippedCards.push(backCards.id)
-    
-} else {
-   
+    // increase count by 1
+    count++;
+    // conditional to stop flipping after 2 selections
+    if(count === 2) {
+        // this.classList.remove('flip');
+        cards.forEach(card =>card.removeEventListener('click', flipCard)); 
+        isAMatch();
+        count=0;
+}
 } 
+
+// TESTING WHETHER A MATCH OR NOT OF 2 CARDS FLIPPED
+ function isAMatch () {
+    let flipped = document.querySelectorAll('.flip');
+    console.log(flipped[0])
+    console.log(flipped[1])
     
+    if (flipped[0].isEqualNode(flipped[1])) {
+        console.log("matched");
+        setTimeout(flipped.forEach(card=>card.classList.add('matched')), 4000);
+        setTimeout(fadeAwayCards, 4000)
+        cards.forEach(card =>card.addEventListener('click', flipCard)); 
+     //if true, score increases by 1 
+    //  score += 1;
+    //  announcement();
+    // remove classlist
+   
+    // resetGame()
+   // if false, not a match
+ } else { 
+ 
+    flipBackCards()
+    console.log("try again!");
+    cards.forEach(card =>card.addEventListener('click', flipCard)); 
+    // resetGame() 
+    // announcement() 
+
+ } 
+
+//  MATCHED TWO FLIPPED CARDS FADES AWAY
+function fadeAwayCards() {
+    let matched = document.querySelectorAll('.matched');
+    matched[0].removeEventListener('click', flipCard)
+    matched[1].removeEventListener('click', flipCard)
+    matched[0].classList.remove('flip');
+    matched[1].classList.remove('flip');
+    
+    cardsFlipped = false
+    // resetGame()
 }
 
-// whether match or not
-// pushed in let flippedCards
-flippedCards.push
-//         // if matched fade away and goes into matched
-matched += 1;
-//         // if not try again alert with red border
-
-   // announcement function run
-        // score function run
-
-// Announcement Function -- results
-function announcement() {
-
+// UNFLIP OF TWO UNMATCHED CARDS
+function flipBackCards() {
+flipsStopped = true
+setTimeout(() => {
+    flipped[0].classList.remove('flip');
+    flipped[1].classList.remove('flip');
+    // resetGame()
+}, 1000)
 }
-    // foreach turn display matched or not
+}
+// ANNOUNCEMENT FUNCTION -- results displays
+
+// function announcement() {
+    
+// foreach turn display matched or not
     // create and grap annoucement display
+// }
+   
 
-// score function
-    // display score number with while loop
+//SCORE FUNCTION -- // display score number 
 
 
-// reset function
+// RESET FUNCTION --RESETS GAME
+
     // clear board
     // clear score
     // clear announcement
     // reset cards randomly
-
-
-
 
 
 
